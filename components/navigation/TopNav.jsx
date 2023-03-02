6c744522046be27e88bf49bf876d6de7c21f97df
mungaben <kamaumungai3742@gmail.com>
2023-03-02 04:45:16 +0300

    navigation gsdp,trasition group

--- components/navigation/TopNav.jsx
+++ components/navigation/TopNav.jsx
@@ -11,11 +11,13 @@ import React from 'react'
 
 import Image from 'next/image'
 import { Link } from 'react-scroll'
+import {CSSTransition,TransitionGroup } from 'react-transition-group'
+
 
 const TopNav = () => {
     return (
 
-        <div class="shadow-xl rounded-b-lg border-b-2 border-b-black  w-full fixed top-0 z-50">
+        <div class="shadow-xl rounded-b-lg border-b-2 border-b-black  w-full fixed top-0 z-50" >
             <div className=' flex  justify-between items-center mx-2  bg-transparent  '>
                 {/* left nav */}
                 <div class=" border-none ml-2 mt-2 rounded-full   ">
