84bf22cea3a6b4c4d0760df2a8b683fef7b26d09
mungaben <kamaumungai3742@gmail.com>
2023-03-05 21:32:18 +0300

    shadow

--- components/Hotincampus.jsx
+++ components/Hotincampus.jsx
@@ -5,7 +5,7 @@ import Homeblog from "@/app/Blog/Homeblog";
 const Hotincampus = () => {
 // const [hoverat, sethoverat] = useState(false);
   return (
-    <div className=" bg-transparent">
+    <div className=" bg-transparent shadow-lg ">
       {/* blop */}
       <Homeblog/>
      
